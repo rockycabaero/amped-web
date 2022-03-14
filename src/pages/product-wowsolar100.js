98f979c7298aad65b2fe6f910d1e2b89a3a4b255
Rocky Cabaero <rockycabaero@gmail.com>
2022-03-15 01:42:59 +0800

    fix font style;

--- src/pages/product-wowsolar100.js
+++ src/pages/product-wowsolar100.js
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import 'twin.macro';
-import { StaticImage } from 'gatsby-plugin-image';
-import Layout from '../components/layout';
-import WowSolarHero from '../components/heroes/wowsolar-hero';
-import WowSolar from '../components/wowsolar';
-import DataAndSpecSummary from '../components/data-and-spec-summary';
-import Button from '../components/_/button';
-import SVGLight from '../assets/ico-light.svg';
-import SVGPaygoController from '../assets/ico-paygo-controller.svg';
-import SVGSolarGrid from '../assets/ico-solar-panel.svg';
-import SVGRadio from '../assets/ico-radio.svg';
-import { SvgCardLandscape, SvgCard } from '../components/svg-card';
-import ExtraInfoFill from '../components/extra-info-fill';
-import LightingSlider from '../components/lighting-slider';
-import BecomeDistributor2 from '../components/sections/become-distributor2';
-import SvgRadio from '../assets/ico-radio.svg';
-import BannerLogo from '../assets/logo-wowsolar100.svg';
+import * as React from "react";
+import "twin.macro";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+import WowSolarHero from "../components/heroes/wowsolar-hero";
+import WowSolar from "../components/wowsolar";
+import DataAndSpecSummary from "../components/data-and-spec-summary";
+import Button from "../components/_/button";
+import SVGLight from "../assets/ico-light.svg";
+import SVGPaygoController from "../assets/ico-paygo-controller.svg";
+import SVGSolarGrid from "../assets/ico-solar-panel.svg";
+import SVGRadio from "../assets/ico-radio.svg";
+import { SvgCardLandscape, SvgCard } from "../components/svg-card";
+import ExtraInfoFill from "../components/extra-info-fill";
+import LightingSlider from "../components/lighting-slider";
+import BecomeDistributor2 from "../components/sections/become-distributor2";
+import SvgRadio from "../assets/ico-radio.svg";
+import BannerLogo from "../assets/logo-wowsolar100.svg";
 
 export default function ProductWowsolar100() {
   const detailRef = React.useRef();
@@ -45,7 +45,7 @@ export default function ProductWowsolar100() {
             }
             caption={
               <span>
-                Amped’s{' '}
+                Amped’s{" "}
                 <span tw="text-primary">best-selling lightning system</span> at
                 an ultra-affordable price for those looking for the highest
                 standard offering
@@ -61,16 +61,16 @@ export default function ProductWowsolar100() {
           />
         </div>
         <div ref={detailRef}>
-          <div tw="px-4 lg:px-0 lg:w-9/12 mx-auto lg:py-48 py-16">
+          <div tw="px-4 lg:px-0 lg:w-9/12 mx-auto lg:py-48 py-16 font-circular-regular">
             <DataAndSpecSummary
               tw="mb-8 lg:mb-24"
               caption={
                 <span>
-                  The <WowSolar tw="text-lg lg:text-px28" model={'100'} />
-                  is designed with 4 or 5 light points, brighter lamps from 140
-                  to 400 Lumens, a bigger 10W solar panel for more phone
-                  charging, and portable torch/radio with an extra-large battery
-                  for longer runtimes between charges.
+                  The <WowSolar tw="text-lg lg:text-px28" model={"100"} /> is
+                  designed with 4 or 5 light points, brighter lamps from 140 to
+                  400 Lumens, a bigger 10W solar panel for more phone charging,
+                  and portable torch/radio with an extra-large battery for
+                  longer runtimes between charges.
                 </span>
               }
               actions={
@@ -86,7 +86,7 @@ export default function ProductWowsolar100() {
             />
             <div tw="mb-8 lg:mb-32">
               <div tw="text-px28 lg:mb-16 mb-8">
-                The <WowSolar tw="text-2xl lg:text-px28" model={'100'} /> comes
+                The <WowSolar tw="text-2xl lg:text-px28" model={"100"} /> comes
                 in two configurations:
               </div>
               <SvgCardLandscape
@@ -97,7 +97,7 @@ export default function ProductWowsolar100() {
                   </>
                 }
                 caption={
-                  'with three overhead lamps and portable torch built into the radio'
+                  "with three overhead lamps and portable torch built into the radio"
                 }
                 svg={
                   <>
@@ -116,7 +116,7 @@ export default function ProductWowsolar100() {
                   </>
                 }
                 caption={
-                  'with four overhead lamps and portable torch built into the radio'
+                  "with four overhead lamps and portable torch built into the radio"
                 }
                 svg={
                   <>
@@ -136,19 +136,19 @@ export default function ProductWowsolar100() {
               <div tw="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:w-1/2 lg:mb-16 mb-8">
                 <SvgCard
                   icon={<SVGPaygoController css={[{ width: 72 }]} />}
-                  content={'20Whr Amped PAYGO Controller'}
+                  content={"20Whr Amped PAYGO Controller"}
                 />
                 <SvgCard
                   icon={<SVGLight css={[{ width: 72 }]} />}
-                  content={'3-Point or 4-Point Lighting Solution'}
+                  content={"3-Point or 4-Point Lighting Solution"}
                 />
                 <SvgCard
                   icon={<SVGSolarGrid css={[{ width: 72 }]} />}
-                  content={'6W Solar Panel'}
+                  content={"6W Solar Panel"}
                 />
                 <SvgCard
                   icon={<SVGRadio css={[{ width: 72 }]} />}
-                  content={'Radio with Portable Torch'}
+                  content={"Radio with Portable Torch"}
                 />
               </div>
               <div tw="text-secondary text-px16">
@@ -164,11 +164,11 @@ export default function ProductWowsolar100() {
           <ExtraInfoFill
             caption={
               <div>
-                The industry leading{' '}
+                The industry leading{" "}
                 <WowSolar
                   tw="text-lg lg:text-px28"
                   model={<span tw="text-primary">100</span>}
-                />{' '}
+                />{" "}
                 has true upgradability that allows you to upgrade the lighting
                 system with a TV or fanin the customer’s home without have to
                 repurchase the controller, lights or phone charging cable.
