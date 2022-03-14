fded2eaf9bfef6096fb9b26b428fc88ad668d800
Rocky Cabaero <rockycabaero@gmail.com>
2022-03-15 02:08:42 +0800

    update to one configuration; fix div width;

--- src/pages/product-wowsolartv24100.js
+++ src/pages/product-wowsolartv24100.js
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import 'twin.macro';
-import { StaticImage } from 'gatsby-plugin-image';
-import Layout from '../components/layout';
-import WowSolarHero from '../components/heroes/wowsolar-hero';
-import WowSolar from '../components/wowsolar';
-import DataAndSpecSummary from '../components/data-and-spec-summary';
-import Button from '../components/_/button';
-import { SvgCardLandscape, SvgCard } from '../components/svg-card';
-import ExtraInfoFill from '../components/extra-info-fill';
-import BecomeDistributor from '../components/sections/become-distributor';
-import SvgLight from '../assets/ico-light.svg';
+import * as React from "react";
+import "twin.macro";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+import WowSolarHero from "../components/heroes/wowsolar-hero";
+import WowSolar from "../components/wowsolar";
+import DataAndSpecSummary from "../components/data-and-spec-summary";
+import Button from "../components/_/button";
+import { SvgCardLandscape, SvgCard } from "../components/svg-card";
+import ExtraInfoFill from "../components/extra-info-fill";
+import BecomeDistributor from "../components/sections/become-distributor";
+import SvgLight from "../assets/ico-light.svg";
 
-import SvgPayGo from '../assets/ico-paygo-controller.svg';
-import SvgSolarPanel from '../assets/ico-solar-panel.svg';
-import SvgExtranalBattery from '../assets/ico-external_battery.svg';
-import SvgTv from '../assets/ico-tv.svg';
-import BannerLogo from '../assets/logo-wowsolartv24100.svg';
+import SvgPayGo from "../assets/ico-paygo-controller.svg";
+import SvgSolarPanel from "../assets/ico-solar-panel.svg";
+import SvgExtranalBattery from "../assets/ico-external_battery.svg";
+import SvgTv from "../assets/ico-tv.svg";
+import BannerLogo from "../assets/logo-wowsolartv24100.svg";
 
 export default function ProductWowsolarTv24100() {
   const detailRef = React.useRef();
@@ -45,7 +45,7 @@ export default function ProductWowsolarTv24100() {
             }
             caption={
               <span>
-                Amped’s{' '}
+                Amped’s{" "}
                 <span tw="text-primary">starting 24” television system </span>
                 for those looking for the brightest television on the market at
                 an unbeatable price
@@ -66,7 +66,7 @@ export default function ProductWowsolarTv24100() {
               tw="mb-16 lg:mb-24"
               caption={
                 <span>
-                  The <WowSolar tw="text-2xl lg:text-px28" model={'TV24100'} />{' '}
+                  The <WowSolar tw="text-2xl lg:text-px28" model={"TV24100"} />{" "}
                   comes with 4 light points at 160 to 400 lumens, a large panel
                   for fast charging and includes innovative SmartPerformance
                   features like Amped Intelligent Power Control - making it the
@@ -86,9 +86,9 @@ export default function ProductWowsolarTv24100() {
               }
             />
             <div tw="mb-16 lg:mb-32">
-              <div tw="text-2xl lg:text-px28 lg:mb-16 mb-8">
-                The <WowSolar tw="text-2xl lg:text-px28" model={'TV24100'} />{' '}
-                comes in two configurations:
+              <div tw="text-2xl lg:text-px28 lg:mb-16 mb-8 w-6/12">
+                The <WowSolar tw="text-2xl lg:text-px28" model={"TV24100"} />{" "}
+                comes in one configuration:
               </div>
               <SvgCardLandscape
                 tw="lg:w-1/2 mb-8"
@@ -98,7 +98,7 @@ export default function ProductWowsolarTv24100() {
                   </>
                 }
                 caption={
-                  'with four overhead lamps, 100Whr total battery and a 40W panel'
+                  "with four overhead lamps, 100Whr total battery and a 40W panel"
                 }
                 svg={
                   <>
@@ -106,24 +106,6 @@ export default function ProductWowsolarTv24100() {
                     <SvgLight css={[{ width: 72 }]} />
                     <SvgLight css={[{ width: 72 }]} />
                     <SvgLight css={[{ width: 72 }]} />
-                  </>
-                }
-              />
-              <SvgCardLandscape
-                tw="lg:w-1/2"
-                title={
-                  <>
-                    <span tw="text-primary">4-Point</span> Lighting System
-                  </>
-                }
-                caption={'with four overhead lamps'}
-                svg={
-                  <>
-                    <SvgLight css={[{ width: 72 }]} />
-                    <SvgLight css={[{ width: 72 }]} />
-                    <SvgLight css={[{ width: 72 }]} />
-                    <SvgLight css={[{ width: 72 }]} />
-                    <SvgSolarPanel css={[{ width: 72 }]} />
                   </>
                 }
               />
@@ -135,23 +117,23 @@ export default function ProductWowsolarTv24100() {
               <div tw="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:w-1/2 lg:mb-16 mb-8">
                 <SvgCard
                   icon={<SvgPayGo css={[{ width: 72 }]} />}
-                  content={'20Whr Amped PAYGO Controller'}
+                  content={"20Whr Amped PAYGO Controller"}
                 />
                 <SvgCard
                   icon={<SvgExtranalBattery css={[{ width: 72 }]} />}
-                  content={'80Whr External Battery'}
+                  content={"80Whr External Battery"}
                 />
                 <SvgCard
                   icon={<SvgLight css={[{ width: 72 }]} />}
-                  content={'4-Point Lights'}
+                  content={"4-Point Lights"}
                 />
                 <SvgCard
                   icon={<SvgSolarPanel css={[{ width: 72 }]} />}
-                  content={'40W Solar Panel'}
+                  content={"40W Solar Panel"}
                 />
                 <SvgCard
                   icon={<SvgTv css={[{ width: 72 }]} />}
-                  content={'24” Solar TV'}
+                  content={"24” Solar TV"}
                 />
               </div>
               <div tw="text-secondary text-px16">
@@ -168,19 +150,19 @@ export default function ProductWowsolarTv24100() {
           <ExtraInfoFill
             caption={
               <div>
-                The industry leading{' '}
+                The industry leading{" "}
                 <WowSolar
                   tw="text-2xl lg:text-px28"
                   model={<span tw="text-primary">TV24100</span>}
-                />{' '}
+                />{" "}
                 system has the option of two high performance accessories - 200
                 Lm metal torch and a radio with a 40 Lumen built in torch. Each
                 accessory has a 7.4Whr battery providing long runtimes between
-                charges. It can be upgrade with a{' '}
+                charges. It can be upgrade with a{" "}
                 <WowSolar
                   tw="text-px28"
                   model={<span tw="text-primary">FAN</span>}
-                />{' '}
+                />{" "}
                 without having to purchase another controller, lights or phone
                 charging cable.
               </div>
